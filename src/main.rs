25a80e9b888fd8748a847e519f39ad4366fbc59f
Victor Song <vms2@rice.edu>
2022-12-01 23:39:49 -0600

    Improve day 1 part 2, start day 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     println!("Day 1 Part 1: {}", day_one_part_one(&args[1])?);
     println!("Day 1 Part 2: {}", day_one_part_two(&args[1])?);
+    println!("Day 2 Part 1: {}", day_two_part_one(&args[2])?);
     Ok(())
+}
+
+fn day_two_part_one(file_path: impl AsRef<str>) -> Result<i32, Box<dyn std::error::Error>> {
+    Ok(0)
 }
 
 fn day_one_part_one(file_path: impl AsRef<str>) -> Result<i32, Box<dyn std::error::Error>> {
@@ -22,16 +27,14 @@ fn day_one_part_two(file_path: impl AsRef<str>) -> Result<i32, Box<dyn std::erro
     Ok(std::fs::read_to_string(file_path.as_ref())?
         .split("\n\n")
         .fold([0; 3], |mut acc, item| {
-            let sum = item.split('\n').flat_map(|num| num.parse::<i32>()).sum();
-            if sum > acc[0] {
-                acc[2] = acc[1];
-                acc[1] = acc[0];
-                acc[0] = sum;
-            } else if sum > acc[1] {
-                acc[2] = acc[1];
-                acc[1] = sum;
-            } else if sum > acc[2] {
-                acc[2] = sum;
+            let mut sum = item
+                .split('\n')
+                .flat_map(|num| num.parse::<i32>())
+                .sum::<i32>();
+            for elem in acc.iter_mut() {
+                if sum > *elem {
+                    std::mem::swap(&mut sum, elem)
+                }
             }
             acc
         })
