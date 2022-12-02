65267e6917e2b0eab75a3513e22180c4c8489ed1
Victor Song <vms2@rice.edu>
2022-12-02 01:41:00 -0600

    Finished day two part 2

--- src/day_two.rs
+++ src/day_two.rs
@@ -1,19 +1,19 @@
 static DAY_2_WINNING_MOVES: phf::Map<&'static str, &'static str> = phf::phf_map! {
-    "A" => "Y",
-    "B" => "Z",
-    "C" => "X",
+    "A" => "Y", // 89 - 65 = 24
+    "B" => "Z", // 90 - 66 = 24
+    "C" => "X", // 88 - 67 = 21
 };
 
 static DAY_2_LOSING_MOVES: phf::Map<&'static str, &'static str> = phf::phf_map! {
-    "A" => "Z",
-    "B" => "X",
-    "C" => "Y",
+    "A" => "Z", // 90 - 65 = 25
+    "B" => "X", // 88 - 66 = 22
+    "C" => "Y", // 89 - 67 = 22
 };
 
 static DAY_2_MATCHES: phf::Map<&'static str, &'static str> = phf::phf_map! {
-    "A" => "X",
-    "B" => "Y",
-    "C" => "Z",
+    "A" => "X", // 88 - 65 = 23
+    "B" => "Y", // 89 - 66 = 23
+    "C" => "Z", // 90 - 67 = 23
 };
 
 static DAY_2_POINTS: phf::Map<&'static str, u32> = phf::phf_map! {
@@ -24,7 +24,7 @@ static DAY_2_POINTS: phf::Map<&'static str, u32> = phf::phf_map! {
 
 pub fn day_two_part_one(file_path: impl AsRef<str>) -> Result<u32, Box<dyn std::error::Error>> {
     Ok(std::fs::read_to_string(file_path.as_ref())?
-        .split('\n')
+        .lines()
         .filter(|round| !round.is_empty())
         .flat_map(|round| -> Result<u32, Box<dyn std::error::Error>> {
             let moves: Vec<&str> = round.split_whitespace().take(2).collect();
@@ -59,6 +59,37 @@ pub fn day_two_part_one(file_path: impl AsRef<str>) -> Result<u32, Box<dyn std::
             }
 
             Ok(sum)
+        })
+        .sum())
+}
+
+pub fn day_two_part_two(file_path: impl AsRef<str>) -> Result<u32, Box<dyn std::error::Error>> {
+    Ok(std::fs::read_to_string(file_path.as_ref())?
+        .lines()
+        .filter(|line| !line.is_empty())
+        .flat_map(|round| -> Result<u32, Box<dyn std::error::Error>> {
+            let moves: Vec<&str> = round.split_whitespace().take(2).collect();
+            let opponent = moves[0];
+            let result = moves[1];
+            let mut score = 0;
+
+            if result == "X" {
+                score += DAY_2_POINTS
+                    .get(DAY_2_LOSING_MOVES.get(opponent).ok_or("invalid move")?)
+                    .ok_or("invalid point")?;
+            } else if result == "Y" {
+                score += DAY_2_POINTS
+                    .get(DAY_2_MATCHES.get(opponent).ok_or("invalid move")?)
+                    .ok_or("invalid point")?
+                    + 3;
+            } else if result == "Z" {
+                score += DAY_2_POINTS
+                    .get(DAY_2_WINNING_MOVES.get(opponent).ok_or("invalid move")?)
+                    .ok_or("invalid point")?
+                    + 6;
+            }
+
+            Ok(score)
         })
         .sum())
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ mod day_one;
 mod day_two;
 
 use day_one::{day_one_part_one, day_one_part_two};
-use day_two::day_two_part_one;
+use day_two::{day_two_part_one, day_two_part_two};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     println!("Day 1 Part 1: {}", day_one_part_one(&args[1])?);
     println!("Day 1 Part 2: {}", day_one_part_two(&args[1])?);
     println!("Day 2 Part 1: {}", day_two_part_one(&args[2])?);
+    println!("Day 2 Part 2: {}", day_two_part_two(&args[2])?);
     Ok(())
 }
