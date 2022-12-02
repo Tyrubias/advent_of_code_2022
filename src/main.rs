0bbe8fc3b0e76efd9c40cfd74093a781e62b10e7
Victor Song <vms2@rice.edu>
2022-12-01 23:29:15 -0600

    Day 1 Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
-    println!("{}", day_one_part_one(&args[1])?);
+    println!("Day 1 Part 1: {}", day_one_part_one(&args[1])?);
+    println!("Day 1 Part 2: {}", day_one_part_two(&args[1])?);
     Ok(())
 }
 
@@ -9,14 +10,31 @@ fn day_one_part_one(file_path: impl AsRef<str>) -> Result<i32, Box<dyn std::erro
         .split("\n\n")
         .map(|input| {
             input
-                .split("\n")
+                .split('\n')
                 .flat_map(|digit| digit.parse::<i32>())
                 .sum()
         })
         .max()
-        .ok_or_else(|| "No maximum")?)
+        .ok_or("No maximum")?)
 }
 
 fn day_one_part_two(file_path: impl AsRef<str>) -> Result<i32, Box<dyn std::error::Error>> {
-    let contents = std::fs::read_to_string(file_path.as_ref())?;
+    Ok(std::fs::read_to_string(file_path.as_ref())?
+        .split("\n\n")
+        .fold([0; 3], |mut acc, item| {
+            let sum = item.split('\n').flat_map(|num| num.parse::<i32>()).sum();
+            if sum > acc[0] {
+                acc[2] = acc[1];
+                acc[1] = acc[0];
+                acc[0] = sum;
+            } else if sum > acc[1] {
+                acc[2] = acc[1];
+                acc[1] = sum;
+            } else if sum > acc[2] {
+                acc[2] = sum;
+            }
+            acc
+        })
+        .iter()
+        .sum())
 }
