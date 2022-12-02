229a8b24b5242b96ad15d6a65072e1213fe2c317
Victor Song <vms2@rice.edu>
2022-12-01 20:51:28 -0600

    Day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    println!("{}", day_one_part_one(&args[1])?);
+    Ok(())
+}
+
+fn day_one_part_one(file_path: impl AsRef<str>) -> Result<i32, Box<dyn std::error::Error>> {
+    Ok(std::fs::read_to_string(file_path.as_ref())?
+        .split("\n\n")
+        .map(|input| {
+            input
+                .split("\n")
+                .flat_map(|digit| digit.parse::<i32>())
+                .sum()
+        })
+        .max()
+        .ok_or_else(|| "No maximum")?)
+}
+
+fn day_one_part_two(file_path: impl AsRef<str>) -> Result<i32, Box<dyn std::error::Error>> {
+    let contents = std::fs::read_to_string(file_path.as_ref())?;
 }
