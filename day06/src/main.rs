8805cfb4b40af0ccc7a11c1b19d453ff6c89bcd6
Victor Song <vms2@rice.edu>
2022-12-10 03:22:12 -0500

    Very dumb day 6 solution

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::VecDeque, env::args_os, fs::read_to_string};
+
+use color_eyre::{eyre::eyre, install, Result};
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    install()?;
+
+    let file_path = args_os()
+        .nth(1)
+        .ok_or_else(|| eyre!("should have one arg"))?;
+    let stream = read_to_string(file_path)?;
+
+    println!("Part 1: {}", find_unique_window_of_size(&stream, 4));
+    println!("Part 2: {}", find_unique_window_of_size(stream, 14));
+
+    Ok(())
+}
+
+fn find_unique_window_of_size(s: impl AsRef<str>, size: usize) -> usize {
+    let mut window = VecDeque::with_capacity(size);
+
+    for (i, c) in s.as_ref().chars().enumerate() {
+        if window.iter().unique().count() == size {
+            return i;
+        }
+        if window.len() >= size {
+            window.pop_front();
+        }
+        window.push_back(c);
+    }
+
+    0
 }
