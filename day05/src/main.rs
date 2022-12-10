95d49b878a3c12e38e161a45aba96b523287c2b9
Victor Song <vms2@rice.edu>
2022-12-09 22:37:30 -0500

    Ship parsing done

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,6 +1,13 @@
-use std::{env::args_os, fs::read_to_string};
+#![forbid(unsafe_code)]
+#![forbid(clippy::unwrap_used, clippy::expect_used, clippy::panic)]
 
-use color_eyre::eyre::eyre;
+use std::{
+    env::args_os,
+    fmt::{self, Debug, Formatter},
+    fs::read_to_string,
+};
+
+use color_eyre::{eyre::eyre, install, Result};
 use itertools::Itertools;
 use nom::{
     branch::alt,
@@ -11,8 +18,41 @@ use nom::{
     Finish, IResult,
 };
 
-#[derive(Debug)]
+fn main() -> Result<()> {
+    install()?;
+
+    let file_path = args_os().nth(1).ok_or_else(|| eyre!("expect a CLI arg"))?;
+    let contents = read_to_string(file_path)?;
+    let (ship, _) = contents
+        .split("\n\n")
+        .take(2)
+        .collect_tuple()
+        .ok_or_else(|| eyre!("should have 2-tuple"))?;
+
+    let mut cargo_lines = Vec::new();
+
+    for line in ship.lines() {
+        if let Ok((_, cargo_line)) = all_consuming(parse_ship_line)(line).finish() {
+            cargo_lines.push(cargo_line)
+        }
+    }
+
+    let cargo_lines = transpose(cargo_lines);
+
+    for cargo_line in &cargo_lines {
+        println!("{cargo_line:?}");
+    }
+
+    Ok(())
+}
+
 struct Crate(char);
+
+impl Debug for Crate {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 fn parse_ship_line(i: &str) -> IResult<&str, Vec<Option<Crate>>> {
     let mut ship_line = Vec::new();
@@ -38,28 +78,21 @@ fn parse_no_crate(i: &str) -> IResult<&str, ()> {
     map(tag("   "), drop)(i)
 }
 
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
-
-    let file_path = args_os().nth(1).ok_or_else(|| eyre!("expect a CLI arg"))?;
-    let contents = read_to_string(file_path)?;
-    let (ship, _) = contents
-        .split("\n\n")
-        .take(2)
-        .collect_tuple()
-        .ok_or_else(|| eyre!("should have 2-tuple"))?;
-
-    let mut cargo_lines = Vec::new();
-
-    for line in ship.lines() {
-        if let Ok((_, cargo_line)) = all_consuming(parse_ship_line)(line).finish() {
-            cargo_lines.push(cargo_line)
-        }
+fn transpose<T>(v: Vec<Vec<Option<T>>>) -> Vec<Vec<T>> {
+    if v.is_empty() {
+        return Vec::new();
     }
 
-    for cargo_line in &cargo_lines {
-        println!("{cargo_line:?}");
-    }
-
-    Ok(())
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .rev()
+                .flat_map(|n| n.next())
+                .flatten()
+                .collect_vec()
+        })
+        .collect_vec()
 }
