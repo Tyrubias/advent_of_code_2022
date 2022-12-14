2884f6d2ff6946ed350b1ee86fc56c8020a12aeb
Victor Song <vms2@rice.edu>
2022-12-13 22:15:42 -0500

    Wrong

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,6 +1,11 @@
-use std::{env::args_os, fs::read_to_string};
+use std::{
+    env::args_os,
+    fs::read_to_string,
+    ops::{Add, AddAssign, Sub, SubAssign},
+};
 
 use color_eyre::{eyre::eyre, install, Result};
+use itertools::Itertools;
 use nom::{
     branch::alt,
     bytes::complete::tag,
@@ -29,15 +34,116 @@ fn main() -> Result<()> {
         })
         .collect::<Result<Vec<_>, _>>()?;
 
-    dbg!(motions);
+    let mut head = Coordinate { x: 0, y: 0 };
+    let mut tail = Coordinate { x: 0, y: 0 };
+
+    let part1 = motions
+        .iter()
+        .flat_map(|&motion| {
+            (0..motion.steps).map(move |_| {
+                head -= motion.direction.into();
+
+                let delta = match (head - tail).into() {
+                    (0, 0) => (0, 0),
+                    (0, 1) | (1, 0) | (0, -1) | (-1, 0) => (0, 0),
+                    (1, 1) | (1, -1) | (-1, 1) | (-1, -1) => (0, 0),
+                    (0, 2) => (0, 1),
+                    (0, -2) => (0, -1),
+                    (2, 0) => (1, 0),
+                    (-2, 0) => (-1, 0),
+                    (2, 1) => (1, 1),
+                    (2, -1) => (1, -1),
+                    (-2, 1) => (-1, 1),
+                    (-2, -1) => (-1, -1),
+                    (1, 2) => (1, 1),
+                    (-1, 2) => (-1, 1),
+                    (1, -2) => (1, -1),
+                    (-1, -2) => (-1, -1),
+                    _ => unreachable!(),
+                };
+
+                tail += delta.into();
+
+                tail
+            })
+        })
+        .unique()
+        .count();
+
+    println!("Part 1: {part1}");
 
     Ok(())
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, Default, Hash, PartialEq, Eq)]
+struct Coordinate {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Coordinate {
+    type Output = Coordinate;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Coordinate {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl AddAssign for Coordinate {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+impl Sub for Coordinate {
+    type Output = Coordinate;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Coordinate {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl SubAssign for Coordinate {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+}
+
+impl From<Direction> for Coordinate {
+    fn from(value: Direction) -> Self {
+        match value {
+            Direction::Right => Coordinate { x: 1, y: 0 },
+            Direction::Up => Coordinate { x: 0, y: -1 },
+            Direction::Left => Coordinate { x: -1, y: 0 },
+            Direction::Down => Coordinate { x: 0, y: 1 },
+        }
+    }
+}
+
+impl From<(i32, i32)> for Coordinate {
+    fn from((x, y): (i32, i32)) -> Self {
+        Coordinate { x, y }
+    }
+}
+
+impl From<Coordinate> for (i32, i32) {
+    fn from(value: Coordinate) -> Self {
+        (value.x, value.y)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 struct Motion {
     direction: Direction,
-    steps: usize,
+    steps: i32,
 }
 
 fn parse_motion(input: &str) -> IResult<&str, Motion> {
